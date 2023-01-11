8eb2a8bd4893d44887600942cf7235c53a9ae11c
NMakarevich <nikoladze529@mail.ru>
2023-01-11 23:33:47 +0300

    fix: fix typeorm

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -2,6 +2,7 @@ import { DataSource } from 'typeorm';
 import 'dotenv/config';
 import * as process from 'process';
 import { User } from './resources/user/entities/user.entity';
+import { Task } from './resources/task/entities/task.entity';
 
 const { env } = process;
 
@@ -12,7 +13,7 @@ const ormConfig = new DataSource({
   username: env.POSTGRES_USER,
   password: env.POSTGRES_PASSWORD,
   database: env.POSTGRES_DB,
-  entities: [User],
+  entities: [User, Task],
   synchronize: false,
   migrations: ['dist/migrations/*.js'],
 });
