7581edf9799542bc0d11cc13a2c0087b55881fee
NMakarevich <nikoladze529@mail.ru>
2023-01-17 21:14:01 +0300

    feat: add swagger api

--- src/resources/auth/dto/login.dto.ts
+++ src/resources/auth/dto/login.dto.ts
@@ -1,9 +1,12 @@
 import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
+  @ApiProperty({ example: 'user' })
   @IsString()
   username: string;
 
+  @ApiProperty({ example: 'password' })
   @IsString()
   password: string;
 }
--- src/resources/user/entities/user.entity.ts
+++ src/resources/user/entities/user.entity.ts
@@ -2,12 +2,15 @@ import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Exclude } from 'class-transformer';
 import { Task } from '../../task/entities/task.entity';
 import { MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('user')
 export class User {
+  @ApiProperty({ example: 'efdfbc10-9e7c-4787-adf3-9aa026f6d8f0' })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty({ example: 'user' })
   @Column()
   username: string;
 
@@ -15,14 +18,21 @@ export class User {
   @Exclude()
   password: string;
 
+  @ApiProperty({
+    example: 'John',
+  })
   @Column()
   @MinLength(2)
   firstName: string;
 
+  @ApiProperty({ example: 'Doe' })
   @Column()
   @MinLength(2)
   lastName: string;
 
+  @ApiProperty({
+    example: 'd5c840cc20bd213aef3f8226a1750b6e.jpeg',
+  })
   @Column({ nullable: true })
   imageName: string;
 
